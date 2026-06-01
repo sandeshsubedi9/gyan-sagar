b5d3b8c017cb1f0a9675fca53be9fbd4ebe04f48
Sandesh <sandeshsubedi2020@gmail.com>
2026-06-01 14:02:29 +0545

    somee diagnostic for speed slow

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,6 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import bcryptjs from "bcryptjs";
 import { z } from "zod";
 
 export const LoginSchema = z.object({
